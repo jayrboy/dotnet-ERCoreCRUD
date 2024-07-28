c58d8cc21fef498a51bf1b166334577ab9694229
jarboy <j.jayox_@hotmail.com>
2024-07-28 21:45:06 +0700

    add ProductController.cs

---
+++ ERCoreCRUD/Controllers/ProductController.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ERCoreCRUD.Data;
+using ERCoreCRUD.Models.db;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ERCoreCRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly NorthwindContext _db;
+
+        public ProductController(NorthwindContext db)
+        {
+            _db = db;
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _db.Products.Any(e => e.ProductId == id);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = _db.Products
+                              .Include(p => p.Category)
+                              .Include(p => p.Supplier);
+            return View(await products.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _db.Products
+                                    .Include(p => p.Category) // join table
+                                    .Include(p => p.Supplier) // join table
+                                    .FirstOrDefaultAsync(m => m.ProductId == id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["SupplierId"] = new SelectList(_db.Suppliers, "SupplierId", "CompanyName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product products)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(products);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["SupplierId"] = new SelectList(_db.Suppliers, "SupplierId", "CompanyName");
+            return View(products);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _db.Products.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["SupplierId"] = new SelectList(_db.Suppliers, "SupplierId", "CompanyName");
+            return View(products);
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("ProductId, ProductName, SupplierId, CategoryId, QuantityPerUnit, UnitPrice, UnitInStock, UnitOnOrder, ReorderLevel, Discontinued")] Product products)
+        {
+            if (id != products.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(products);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CategoryId"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["SupplierId"] = new SelectList(_db.Suppliers, "SupplierId", "CompanyName");
+            return View(products);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _db.Products
+                                    .Include(p => p.Category) // join table
+                                    .Include(p => p.Supplier) // join table
+                                    .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _db.Products.FindAsync(id);
+
+            // Check if the product is found
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            _db.Products.Remove(products);
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
